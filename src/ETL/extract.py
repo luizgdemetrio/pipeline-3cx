0af5e361c17d4d2af8c70cfd73589f35b687c5bc
Luiz <lgds.demtrio@gmail.com>
2024-12-24 11:56:51 -0300

    :sparkles: Feat: Adicionado repetição para aguardar download do relatório

--- src/ETL/extract.py
+++ src/ETL/extract.py
@@ -27,19 +27,48 @@ driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()
 # Exemplo de navegação
 driver.get(report_login_url)
 
-# Código para interagir com a página
+# Código para fazer login
 element = driver.find_element(By.ID, 'loginInput').send_keys(login_ramal)
 element = driver.find_element(By.ID, 'passwordInput').send_keys(password_ramal)
-time.sleep(10)
+time.sleep(5)
 element = driver.find_element(By.ID, 'submitBtn').click()
 
+# Código para remover o arquivo anterior
+if os.path.exists("data/call_reports.csv"):
+    os.remove("data/call_reports.csv")
+else:
+    print('Relatório não encontrado')
+
+
+# Código para extrair relatório
 driver.get(office_dashboard)
 time.sleep(5)
 element = driver.find_element(By.XPATH, "//a[@routerlink='/office/reports']").click()
-time.sleep(10)
+time.sleep(5)
 element = driver.find_element(By.XPATH, "//a[@data-qa='call-reports-link']").click()
-time.sleep(10)
+time.sleep(5)
 element = driver.find_element(By.XPATH, "//button[@class='btn btn-primary']").click()
 
+
 # Fecha o WebDriver
-driver.quit()
+tempo_esperado = int(0)
+tempo_maximo = int(30)
+while not os.path.exists('data/call_reports.csv') and tempo_esperado < tempo_maximo:
+    time.sleep(1)
+    tempo_esperado += 1
+    print(f"Aguarde: {tempo_esperado}s")
+if os.path.exists('data/call_reports.csv'):
+    print(f"Relatório extraido com sucesso, salvo em: {os.path.join(os.getcwd(), 'data/call_reports.csv')}")
+    driver.quit()
+    True
+else:
+    print(f"O relatório não foi encontrado após {tempo_maximo} segundos.")
+    driver.quit()
+    False
+
+"""if os.path.exists("data/call_reports.csv"):
+    print(f"Relatório extraido com sucesso, salvo em: {os.path.join(os.getcwd(), 'data/call_reports.csv')}")
+    driver.quit()
+else:
+    print("Relatório não extraido.\nTentar novamente")
+"""
